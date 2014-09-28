22f62768591776ecb41bb7131d3a32eb5be02c4f
Perry Rosenstein <perryr@gmail.com>
2014-09-27 21:42:35 -0700

    Testing a new empty line

--- client/routes.js
+++ client/routes.js
@@ -41,6 +41,7 @@ Router.map(function() {
 	  if (!decision || !choice)
 	    return {error: true};
 
+
 	  $('meta[property="og:title"]').attr('content', choice.title);
 	  $('meta[property="og:url"]').attr('content', window.location.href);
 	  $('meta[property="og:image"]').attr('content', window.location.origin + "/images/" + choice.photo);
