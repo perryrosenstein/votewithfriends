568ce7b66c101a0fa142f8cb73f3cfa5908d0293
Avital Oliver <avital@thewe.net>
2014-10-14 21:01:17 -0700

    Get a smoother redirect flow for /connect

--- client/connect.js
+++ client/connect.js
@@ -1,16 +1,16 @@
 Template.connect.rendered = function() {
-	/* Vertically align the content */
-	var containerHeight = $('.home-container').height();
-	var screenHeight = $(window).height();
-	$('.home-container').css('margin-top', (screenHeight - containerHeight) / 2);
-	
-	$("#connect-with-facebook").click(function() {
-		FB.login(function(response) {
-		   if (response.authResponse) {
-				window.location.href = '/ballot/' + response.authResponse.userID;		
-		   } else {
-             alert('Login failed. Please try again.');
-		   }
-		 });
-	});
+  /* Vertically align the content */
+  var containerHeight = $('.home-container').height();
+  var screenHeight = $(window).height();
+  $('.home-container').css('margin-top', (screenHeight - containerHeight) / 2);
+
+  $("#connect-with-facebook").click(function() {
+    FB.login(function(response) {
+      if (response.authResponse) {
+	window.location.href = '/ballot/' + response.authResponse.userID;
+      } else {
+        alert('Login failed. Please try again.');
+      }
+    });
+  });
 };
