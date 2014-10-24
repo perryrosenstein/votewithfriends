3fe55d9152138d6f7e350b7eeff50ffd79cbe3e1
Perry Rosenstein <perryr@gmail.com>
2014-10-24 12:55:28 -0700

    add go:image:width&height properties, fix on share button on /guide page

--- client/routes.js
+++ client/routes.js
@@ -32,8 +32,11 @@ Router.map(function() {
 	data: function() {
 		var ogTitle = this.params.firstName + " " + this.params.lastName + "'s Voter Guide";
 		var ogImage = "https://graph.facebook.com/" + this.params.fbid + "/picture?height=400&width=400";
+		alert(ogImage);
 		$('meta[property="og:title"]').attr('content', ogTitle);
 		$('meta[property="og:image"]').attr('content', ogImage); 
+		$('meta[property="og:image:width"]').attr('content', 400);
+		$('meta[property="og:image:height"]').attr('content', 400);
 		$('meta[property="og:url"]').attr('content', window.location.href);		       
 		return this.params;
 	}
